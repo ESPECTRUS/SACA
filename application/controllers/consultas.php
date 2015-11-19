afdd768b426bd1655c0a9e2b806471abba119026
DEV-UET <dev.uet2015@gmail.com>
2015-11-19 12:46:48 -0400

    sesiones

--- application/controllers/consultas.php
+++ application/controllers/consultas.php
@@ -9,25 +9,32 @@ class Consultas extends CI_Controller
     	 $this->load->helper('form');
     	 $this->load->helper('date');
       	 $this->load->helper('url');
-    	 $this->load->database('default');
+        if(!$this->session->userdata('logueado'))
+        {
+          redirect('login');
+        }
     }
 
         public function busqueda_proyecto()
     {
+           $data2['nombre'] = $this->session->userdata('nombre');
+           $data2['apellidop'] = $this->session->userdata('apellidopat');
+           $data2['apellidom'] = $this->session->userdata('apellidomat');
+           $data2['nic']  =$this->session->userdata('nic_usu');    	 $this->load->database('default');
         $query = $this->input->post('npr_dte');
         $a = $this->input->post('man_dte');
         if($query)
         {
             $query = $this->db->query("CALL busca_archivo_proy('%".$query."%','')");
             $data['result'] = $query;
-        $this->load->view('Consultas/grilla_proyecto',$data);            
+        $this->load->view('Consultas/grilla_proyecto',$data,$data2);            
         }
         else
         if($a)
         {
             $query = $this->db->query("CALL busca_archivo_proy('','%".$a."%')");
             $data['result'] = $query;
-        $this->load->view('Consultas/grilla_proyecto',$data);
+        $this->load->view('Consultas/grilla_proyecto',$data,$data2);
         }
         else
         {
--- application/controllers/solicitud.php
+++ application/controllers/solicitud.php
@@ -5,23 +5,47 @@ class Solicitud extends CI_Controller
 	public function __construct()
 	{
         parent::__construct();
+        $this->load->model('usuarios_model');
+        if(!$this->session->userdata('logueado'))
+        {
+        	redirect('login');
+        }
     }
 	public function index()
 	{
-		$this->load->view('solicitud_archivo/solicitud_view');
+       	 $data2['nombre'] = $this->session->userdata('nombre');
+       	 $data2['apellidop'] = $this->session->userdata('apellidopat');
+       	 $data2['apellidom'] = $this->session->userdata('apellidomat');
+       	 $data2['nic']  =$this->session->userdata('nic_usu');
+		$this->load->view('solicitud_archivo/solicitud_view',$data2);
 	}
 	public function retiro()
 	{
-		$this->load->view('solicitud_archivo/retiro_view');
+
+         	 $data2['nombre'] = $this->session->userdata('nombre');
+         	 $data2['apellidop'] = $this->session->userdata('apellidopat');
+         	 $data2['apellidom'] = $this->session->userdata('apellidomat');
+         	 $data2['nic']  =$this->session->userdata('nic_usu');
+		$this->load->view('solicitud_archivo/retiro_view',$data2);
 	}
 	
 	public function devolucion()
 	{
-		$this->load->view('solicitud_archivo/devoluciones_view');
+
+         	 $data2['nombre'] = $this->session->userdata('nombre');
+         	 $data2['apellidop'] = $this->session->userdata('apellidopat');
+         	 $data2['apellidom'] = $this->session->userdata('apellidomat');
+         	 $data2['nic']  =$this->session->userdata('nic_usu');
+		$this->load->view('solicitud_archivo/devoluciones_view',$data2);
 	}
 	public function vista()
 	{
-		$this->load->view('solicitud_archivo/solicitud-vista');
+
+         	 $data2['nombre'] = $this->session->userdata('nombre');
+         	 $data2['apellidop'] = $this->session->userdata('apellidopat');
+         	 $data2['apellidom'] = $this->session->userdata('apellidomat');
+         	 $data2['nic']  =$this->session->userdata('nic_usu');
+		$this->load->view('solicitud_archivo/solicitud-vista',$data2);
 	}
 		
 }
